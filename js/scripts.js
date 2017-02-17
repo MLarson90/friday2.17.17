d0a721751ec69ea3c279cbdbcc9d143394d03af0
Max Larson <mr.m.larson90@gmail.com>
2017-02-17 11:46:05 -0800

    finished de bugging java script to allow if statements to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
 $(document).ready(function(){
-  var question1 = $("input:radio[name=question1]:checked").val();
-  var question2 = $("input:radio[name=question2]:checked").val();
-  var question3 = $("input:radio[name=question3]:checked").val();
-  var question4 = $("input:radio[name=question4]:checked").val();
-  $("form#survey").submit(function(event){
+
+
+
+  $("#survey").submit(function(event){
     event.preventDefault();
+    var question1 = $("input:radio[name=question1]:checked").val();
+    var question2 = $("input:radio[name=question3]:checked").val();
+    var question3 = $("input:radio[name=question4]:checked").val();
+    var question4 = $("input:radio[name=question4]:checked").val();
+
+    console.log(question1);
     if(!question1 || !question2 || !question3 || !question4){
-      alert("Please finish filling out questions!")
+    alert("Please finish filling out questions!");
+  }else if ((question1 === "a" || question1 === "b") && (question2 === "b" || question2 === "c")){
+    alert("Java!");
     };
+
   });
 });
