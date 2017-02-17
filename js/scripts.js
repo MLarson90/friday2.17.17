e9e084cc996b5e22a304321427a35a49749f60ab
Max Larson <mr.m.larson90@gmail.com>
2017-02-17 13:36:45 -0800

    add custom name to survey answers

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,24 @@ $(document).ready(function(){
     var question2 = $("input:radio[name=question3]:checked").val();
     var question3 = $("input:radio[name=question4]:checked").val();
     var question4 = $("input:radio[name=question4]:checked").val();
+    var nameInput = $("input#name").val();
+    $(".student").append(nameInput);
 
-    console.log(question1);
     if(!question1 || !question2 || !question3 || !question4){
     alert("Please finish filling out questions!");
   }else if ((question1 === "a" || question1 === "b") && (question2 === "b" || question2 === "c")){
-    ("#java!").show();
-    }
+    $("#cc").hide();
+    $("#php").hide();
+    $("#java").show();
+  }else if ((question1 === "b" || question1 === "c") && question2 === "c"){
+    $("#php").hide();
+    $("#java").hide();
+    $("#cc").show();
+  }else if ((question1 === "a" || question1 === "c") && (question2 === "a" || question2 === "b")){
+    $("#java").hide();
+    $("#cc").hide();
+    $("#php").show();
+  };
 
   });
 });
